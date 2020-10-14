39d98e6a9476c913f55f5ee128b1f65cdda66db8
Dirceu Henrique <dhnasci@hotmail.com>
2020-10-14 19:10:40 -0400

    Working with Exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import 'express-async-errors'
 import path from 'path';
 import './database/connection'
 import routes from './routes'
+
+import errorHandler from './errors/handler'
 
 const app = express();
 
@@ -9,6 +12,7 @@ app.use(express.json());
 
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHandler);
 
 // Rota /users
 // Recurso > users
